0954ccf3cdadd86a009bad6ece4f12da1763326f
Saurav <spike.samantray@gmail.com>
2022-09-20 21:42:17 +0530

    day5 update

--- day6/user-management-service/app/api/addresses_api.py
+++ day6/user-management-service/app/api/addresses_api.py
@@ -1,12 +1,32 @@
+from flask import request
 from flask_restful import Resource
+from ..database import get_db_connection, close_db_connection, commit_and_close_db_connection
+from ..database import address_db
+from ..schemas.address_schema import AddressSchema
+from ..exceptions import InvalidAddressPayload
+from ..models.address import Address
+
+address_schema = AddressSchema()
 
 class AddressesApi(Resource):
 
 	def get(self):
-		return {'message': 'Hello GET'}
+		conn = get_db_connection()
+		addresses = address_db.get_addresses(conn)
+		close_db_connection(conn)
+		return addresses
 
 	def post(self):
-		return {'message': 'Hello POST'}, 201
+		errors = address_schema.validate(request.json)
+		print("errors: "+str(errors))
+		if errors:
+			raise InvalidAddressPayload(errors, 400)
+		#user_dict['password'] = flask_bcrypt.generate_password_hash(user_dict['password'])
+		conn = get_db_connection()
+		address_db.create_address(conn, Address.from_json(request.json))
+		addresses = address_db.get_addressesa(conn)
+		commit_and_close_db_connection(conn)
+		return addresses, 201
 
 	def put(self):
 		return {'message': 'Hello PUT'}
