0268db80382d0bd564b61c3f70939c98c669a953
Saurav <spike.samantray@gmail.com>
2022-09-23 07:54:34 +0530

    day7 update

--- day7/user-management-service/app/database/address_db.py
+++ day7/user-management-service/app/database/address_db.py
@@ -1,4 +1,5 @@
 from app.exceptions import AddressNotFoundException
+from .user_db import get_user_details
 
 def get_addresses(conn, user_id):
 	results = conn.execute('SELECT * FROM address where user_id = ?', (str(user_id),)).fetchall()
@@ -6,6 +7,7 @@ def get_addresses(conn, user_id):
 	return results
 
 def create_address(conn, address, user_id):
+	get_user_details(conn, user_id)
 	conn.execute('INSERT INTO address(address_line_1, city, state, pin, user_id) VALUES (?, ?, ?, ?, ?)',(address.address_line_1, address.city, address.state, address.pin, str(user_id)))
 
 def get_address_details(conn, id, user_id):
@@ -15,7 +17,9 @@ def get_address_details(conn, id, user_id):
 	return dict(result) if result != None else None
 
 def update_address_details(conn, id, address, user_id):
+	get_user_details(conn, user_id)
 	conn.execute('UPDATE address SET address_line_1 = ?, city = ?, state = ?, pin = ? where id = ? and user_id = ? ', (address.address_line_1, address.city, address.state, address.pin, str(id), str(user_id)))
 
 def delete_address(conn, id, user_id):
+	get_user_details(conn, user_id)
 	conn.execute('DELETE from address where id = ? and user_id = ?', (str(id, user_id)))       
