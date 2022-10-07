8cd38404ce419268a605b63cf3ca6adfaa855a77
Saurav <spike.samantray@gmail.com>
2022-10-07 08:07:54 +0530

    day14 update

---
+++ day14/user-management-service/server.py
@@ -0,0 +1,11 @@
+from app import app, db
+from app.api.users_search_api import UsersSearchApi
+from app.api.users_api import UsersApi
+from app.api.user_api import UserApi
+from app.api.addresses_api import AddressesApi
+from app.api.address_api import AddressApi
+from app.api.auth_api import AuthApi, RegisterApi
+from app.callbacks import jwt_callbacks
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
