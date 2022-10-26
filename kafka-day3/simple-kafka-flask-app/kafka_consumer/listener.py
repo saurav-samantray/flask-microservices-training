095f50ab23b07feb6fc80cda7034506d7e457107
saurav-cp <saurav@cultrplus.com>
2022-10-26 07:30:52 +0530

    kafka day 3

--- kafka-day3/simple-kafka-flask-app/kafka_consumer/listener.py
+++ kafka-day3/simple-kafka-flask-app/kafka_consumer/listener.py
@@ -1,10 +1,11 @@
 from kafka import KafkaConsumer
 
-# To consume latest messages and auto-commit offsets
-consumer = KafkaConsumer('first-topic',
+
+def read(topic_name):
+    # To consume latest messages and auto-commit offsets
+    consumer = KafkaConsumer(topic_name,
                          group_id='demo-group',
                          bootstrap_servers=['localhost:9092'])
-def read():
     for message in consumer:
         # message value and key are raw bytes -- decode if necessary!
         # e.g., for unicode: `message.value.decode('utf-8')`
